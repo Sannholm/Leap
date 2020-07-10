0c6a580ce4f472b57f47f40d9b35e9da3aa72fec
Benjamin Sannholm <benjamin@sannholm.se>
2020-07-10 20:13:22 +0200

    First version of jumping and platforms

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject followedPlayer;
+    public Vector3 offset;
+    public float smoothTime;
+    
+    private Vector3 followVelocity;
+
+    void Start()
+    {
+        transform.position = getTargetPos();
+    }
+
+    private Vector3 getTargetPos()
+    {
+        Vector3 targetPos = followedPlayer.transform.position + offset;
+        targetPos = new Vector3(Mathf.Max(0, targetPos.x), targetPos.y, targetPos.z);
+        return targetPos;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 currentPos = transform.position;
+        Vector3 targetPos = getTargetPos();
+        Vector3 p = Vector3.SmoothDamp(currentPos, targetPos, ref followVelocity, smoothTime);
+        
+        transform.position = p;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 3f;
+    public float jumpSpeed = 10f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnJump()
+    {
+        Debug.Log("Jump");
+        rb.AddForce(Vector3.up * jumpSpeed, ForceMode.VelocityChange);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + Vector3.right * movementSpeed * Time.fixedDeltaTime);
+    }
+}
