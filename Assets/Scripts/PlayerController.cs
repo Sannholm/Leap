5886d60653dcee06a9c9c5e14cd1f52158268b68
Benjamin Sannholm <benjamin@sannholm.se>
2020-07-13 22:09:38 +0200

    Kinda working variable jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,22 +6,63 @@ public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 3f;
     public float jumpSpeed = 10f;
+    public float onGroundMargin = 0.2f;
+    public LayerMask groundLayerMask;
 
     private Rigidbody rb;
+    private CapsuleCollider coll;
+
+    private float currSpeed;
+
+    private ISet<Platform> visitedPlatforms = new HashSet<Platform>();
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        coll = GetComponent<CapsuleCollider>();
+        
+        currSpeed = movementSpeed;
     }
 
     void OnJump()
     {
-        Debug.Log("Jump");
-        rb.AddForce(Vector3.up * jumpSpeed, ForceMode.VelocityChange);
+        Platform platform = GetOnPlatform();
+        if (platform != null)
+        {
+            rb.AddForce(Vector3.up * platform.GetJumpVelocity().y, ForceMode.VelocityChange);
+            currSpeed = platform.GetJumpVelocity().x;
+        }
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + Vector3.right * movementSpeed * Time.fixedDeltaTime);
+        rb.velocity = new Vector3(currSpeed, rb.velocity.y, 0);
+        //rb.MovePosition(rb.position + Vector3.right * currSpeed * Time.fixedDeltaTime);
+    }
+
+    private Platform GetOnPlatform()
+    {
+        Vector3 center = coll.bounds.center + Vector3.down * (coll.height/2 - coll.radius + onGroundMargin);
+        Collider[] colliders = Physics.OverlapSphere(center, coll.radius, groundLayerMask, QueryTriggerInteraction.Ignore);
+        Platform foundPlatform = null;
+        foreach (var collider in colliders)
+        {
+            Platform platform = collider.gameObject.GetComponentInParent<Platform>();
+            if (platform != null)
+            {
+                foundPlatform = platform;
+                break;
+            }
+        }
+        return foundPlatform;
+    }
+
+    void OnTriggerStay(Collider collider) {
+        Platform platform = collider.gameObject.GetComponentInParent<Platform>();
+        if (platform != null && rb.velocity.y <= 0 && platform.isAbovePlatform(coll.bounds.min.y) && !visitedPlatforms.Contains(platform)) {
+            visitedPlatforms.Add(platform);
+            platform.OnPlayerLand();
+            currSpeed = movementSpeed;
+        }
     }
 }
